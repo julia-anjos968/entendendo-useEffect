72199bcfec2662b298c9af0ab95cc15b005d4c6e
julia-anjos968 <julia.r.anjos@aluno.senai.br>
2026-04-27 16:21:56 -0300

    feat: termina a atividade

--- meu-app/src/App.jsx
+++ meu-app/src/App.jsx
@@ -1,42 +1,16 @@
-import { useEffect, useState } from 'react';
-import './App.module.css';
+import { Exercicio1 } from './Exercicio1';
+import { Exercicio2 } from './Exercicio2';
+import { Exercicio3 } from './Exercicio3';
+import styles from './App.module.css';
 
 function App() {
-    const [likes, setLikes] = useState(0);
-    const [input, setInput] = useState('Nada');
-
-    function darOLike() {
-        setLikes((prevLikes) => prevLikes + 1);
-    }
-
-    /*
-
-    useEffect / Efeito Colateral
-        - Ele vai ser chamado quando ...
-
-            1) Qualquer estado for alterado
-            2) Quando UM estado determinado for alterado
-            3) Quando a tela carregar
-
-*/  useEffect(() => {
-        console.log("Oi, mudou alguma coisa")
-})
-    useEffect(() => {
-        alert("Olá, seja bem-vindo(a) ao site")
-    }, [])
-
-    useEffect(() => {
-         if(likes > 9)
-         alert(`Parabéns, você recebeu ${likes} likes`);
-     }, [likes]);
-
     return (
-        <div>
-            <p>Quantidade de Likes: {likes}</p>
-            <button onClick={darOLike}>Like</button>
-
-            <p>0 que você digitou no Input {input}</p>
-            <input onChange={(event) => setInput(event.target.value)} />
+        <div className={styles.app}>
+            <Exercicio1 />
+            <hr />
+            <Exercicio2 />
+            <hr />
+            <Exercicio3 />
         </div>
     );
 }
